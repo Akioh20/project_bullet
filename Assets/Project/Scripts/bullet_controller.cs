51d7b9e6a65a94127f11c51f155e2755ebec3290
Victor <victor.romero.2@enti.cat>
2021-11-05 19:56:32 +0100

    Bullet movement

--- Assets/Project/Scripts/bullet_controller.cs
+++ Assets/Project/Scripts/bullet_controller.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class bullet_controller : MonoBehaviour
 {
+    public float movSpeed = 5f;
+    public float turnRatio = 60f;
+    Vector3 currentMovement = Vector3.right;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +16,29 @@ public class bullet_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Vector2 mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-            if (Physics2D.OverlapCircle(mousePos, 0.05f) == GetComponent<Collider2D>())
-            {
-                Debug.Log("Clicked collider");
-            }
-        }
+        ////// GET THE MOUSE IN WORLD SPACE
+        Vector2 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        ////// GET THE DIRECTION TO THE MOUSE
+        Vector3 targetMovement = mousePos - new Vector2(transform.position.x, transform.position.y);
+        targetMovement = targetMovement.normalized;
+
+        ////// ROTATION CHANGE
+        float angle = Vector2.SignedAngle(currentMovement, targetMovement);
+        //Angle that can be covered in 1 second
+        float rotationInOneFrame = turnRatio * Time.deltaTime;
+        angle = Mathf.Clamp(angle, -rotationInOneFrame, rotationInOneFrame);
+        this.transform.rotation = this.transform.rotation * Quaternion.AngleAxis(angle, Vector3.forward);
+        currentMovement = this.transform.right;
+
+        ////// POSITION CHANGE
+        //Distance that can be covered in 1 second
+        float movementInOneFrame = movSpeed * Time.deltaTime;
+
+        this.transform.position += currentMovement.normalized * movementInOneFrame;
+
+
     }
 
     private void FixedUpdate()
