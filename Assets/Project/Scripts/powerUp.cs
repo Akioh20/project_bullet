aad1961b8594f1f0024a424b015ce917ffd80e83
Laia <laia.rodriguez@enti.cat>
2021-11-16 14:33:05 +0100

    Script Power Up

--- Assets/Project/Scripts/powerUp.cs
+++ Assets/Project/Scripts/powerUp.cs
@@ -11,11 +11,25 @@ public class powerUp : MonoBehaviour
         if (col.gameObject.name == "Character" && gameObject.tag == "Fire")
         {
             Debug.Log("UN POWER UP DE FOC HA TOCAT LA BALA, APLICA MÉS DANY.");
+            Destroy(gameObject);
+
+            //AQUI DISCUTIR SI UNA BALA POT TENIR MES D'UN POWER UP I COM HO FEM
+
+            //UN POWER UP TE TEMPS?
+            //TEMPS PER AGAFAR-LO O S'ACTIVA DESPRES DE AGAFAR EL PU?
+
+
+            //UN POWER UP TE UNA SOLA BONIFICACIO?
+
+            //PARLAR SOBRE ELS POWER UPS
+
+            //DESTRUIR SERA MES EFECTIU X EL RENDIMENT
         }
 
         if (col.gameObject.name == "Character" && gameObject.tag == "Shield")
         {
             Debug.Log("UN POWER UP DE ESCUT HA TOCAT LA BALA, APLICA MÉS RESISTENCIA.");
+            Destroy(gameObject);
         }
     }
 }
