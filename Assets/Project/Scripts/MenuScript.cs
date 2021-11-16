07709728dd28759e430793ede8c31be781007a7f
Laia <laia.rodriguez@enti.cat>
2021-11-16 13:37:20 +0100

    Hierarchy correct now. Fixed menu now. Demo button goes into Scene_1

---
+++ Assets/Project/Scripts/MenuScript.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Canvas menuEnter;
+    public Canvas menuSelect;
+    public Canvas menuLevel;
+    public Canvas menuOptions;
+
+
+    void Start()
+    {
+        menuSelect.GetComponent<Canvas>().enabled = false;
+        menuLevel.GetComponent<Canvas>().enabled = false;
+        menuOptions.GetComponent<Canvas>().enabled = false;
+
+        menuEnter.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void GoToSelect()
+    {
+        menuEnter.GetComponent<Canvas>().enabled = false;
+        menuLevel.GetComponent<Canvas>().enabled = false;
+        menuOptions.GetComponent<Canvas>().enabled = false;
+
+        menuSelect.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void GoToLevel()
+    {
+        menuEnter.GetComponent<Canvas>().enabled = false;
+        menuSelect.GetComponent<Canvas>().enabled = false;
+        menuOptions.GetComponent<Canvas>().enabled = false;
+
+        menuLevel.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void GoToOptions()
+    {
+        menuEnter.GetComponent<Canvas>().enabled = false;
+        menuLevel.GetComponent<Canvas>().enabled = false;
+        menuSelect.GetComponent<Canvas>().enabled = false;
+
+        menuOptions.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void ExitOnClick()
+    {
+        Debug.Log("Adeeeu");
+        Application.Quit();
+    }
+
+    public void GoToDemo()
+    {
+        Debug.Log("Salta a la escena de la presentacio");
+        SceneManager.LoadScene("Scene_1", LoadSceneMode.Single);
+    }
+
+}
