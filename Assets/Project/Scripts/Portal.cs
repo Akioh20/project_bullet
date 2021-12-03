61296e6c4c869a27c1128756a5b72c5c834295af
LaiaRodri <laia.rodriguez@enti.cat>
2021-12-03 14:49:52 +0100

    Audio Manager.

--- Assets/Project/Scripts/Portal.cs
+++ Assets/Project/Scripts/Portal.cs
@@ -14,11 +14,15 @@ public class Portal : MonoBehaviour
         {
             Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
             col.gameObject.transform.position = portalFinal.transform.position;
+
+
+            //FindObjectOfType<audioManager>().Play("BounceSound");
         }
 
         if (col.gameObject.name == "Character" && gameObject.tag == "PortalFin")
         {
             Debug.Log("NOOO! PER AQUEST PORTAL SURTS");
+            //AQUI HI PODRIA HABER UN SOROLL ROLLO REBOT
         }
     }
 }
