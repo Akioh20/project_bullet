f057c0e78684d2ce42b1a286b21dd51028610680
LaiaRodri <laia.rodriguez@enti.cat>
2021-12-17 18:37:13 +0100

    Prefab Main Camera

--- Assets/Project/Scripts/Walls/WallBouncy.cs
+++ Assets/Project/Scripts/Walls/WallBouncy.cs
@@ -7,37 +7,12 @@ public class WallBouncy : MonoBehaviour
     //////VARIABLES
     public ParticleSystem bounceParticles;
 
-    int zoom = 20;
-    int normal = 60;
-    float smooth = 5; //this will make the transition not so abrupt
-
-    private bool isZoomed;
-
-    ZoomIn zoomIn;
-
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Instantiate(bounceParticles, new Vector3(collision.transform.position.x, collision.transform.position.y , collision.transform.position.z), Quaternion.identity);
-            zoomIn.bZoom();
-            //isZoomed = !isZoomed; //this tell the program that when the player collides, the variable must be opposite as what is set in the init
+            Camera.main.GetComponent<ZoomIn>().SetZoom(4f); //Zoom Camera
         }
-
-        zoomIn.ControlCamera();
-
-        /*
-        if (isZoomed)
-        {
-            //the boolean is true, will make the zoom
-            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
-        }
-        else
-        {
-            //if not it will return to normal
-            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normal, Time.deltaTime * smooth);
-        }
-         */
     }
-
 }
